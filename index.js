ef581105df24a83dbc422876b3f09ef769996c41
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-08 12:49:30 -0800

    update: added async to db connect

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 /* Where our imports live and how we connect to our database */
 const express = require("express");
 const router = require("./router");
@@ -10,20 +9,25 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
-mongoose
-.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-app.listen(PORT, async () => {
-    console.log(`Listening on Port ${PORT}`);
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit the process if connection fails
+  }
+}
+
+// Call the connectToDatabase function to establish connection
+connectToDatabase();
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
 });
