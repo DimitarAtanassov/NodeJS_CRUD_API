b601e95eef634b546d1bbf3f18dc699a06b14ab2
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-08 11:06:33 -0800

    Added error handling to createUser crud function

--- controllers/user.js
+++ controllers/user.js
@@ -17,13 +17,26 @@ const getUsers = async (req, res) => {
 
 // Creates a User
 const createUser = async (req,res) => {
-    
-    const newUser = new User({
-        username : req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    });
-    
+    try {
+        // Create newUser
+        const newUser = new User({
+            username : req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+        
+        // Save newUser
+        await newUser.save();
+        
+        // Return the newly created user a JSON res
+        res.json(newUser);
+    } catch (error) {
+        // Error handling during for user creation done here
+
+        // Return 500 for internal server error with the error message
+        res.status(500).send(error.message);
+    }
+
     await newUser.save();
     res.json(newUser);
 };
