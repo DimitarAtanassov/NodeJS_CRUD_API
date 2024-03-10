8f5982340908ad16b5739f012ef02226aa4b96d6
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-10 13:48:46 -0700

    Updated verifyToken logic

--- utils/auth.js
+++ utils/auth.js
@@ -1,20 +1,18 @@
-const jwt = req("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 require('dotenv').config();
 
-const verifyToken = (req,res,next) => {
-    const token = req.session.token;
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization;
 
-    if (!token)
-    {
-        return res.status(403).json({message: "No JWT found!"});
+    if (!token) {
+        return res.status(403).json({ message: "No JWT found!" });
     }
 
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-        if (err)
-        {
-            return res.status(401).json({message:"Unauthorized"});
+        if (err) {
+            return res.status(401).json({ message: "Unauthorized" });
         }
-        req.userId = decoded.id;
+        req.userId = decoded.userId;
         next();
     });
 };
