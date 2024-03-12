c43c7c1668eddb593aadaf149cc58d32ebf4885f
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-12 11:12:54 -0700

    Updated routes

--- router.js
+++ router.js
@@ -14,32 +14,24 @@ const {
     getAllUsers,
     login
 } = require("./controllers/user");
-const {createJobApp,getAllJobApps} = require('./controllers/jobApp')
+const { createJobApp, getAllJobApps } = require('./controllers/jobApp');
 const verifyToken = require('./utils/auth');
 
 router.get("/", async (req,res) => {
     res.send("CRUD API FOR MONGODB!");
 });
 
-
-
 router.post("/api/users", createUser);
 router.post("/api/users/login", login);
-// Authentication middleware 
-router.use(verifyToken);
-router.put("/api/users/:userID", updateUserPassword);
-router.delete("/api/users/:userID", deleteUser);
-router.get("/api/users/:userID", getUserById);
-router.get("/api/users/", getAllUsers);
+
+// Protected routes requiring token authentication middleware
+router.put("/api/users/:userID", verifyToken, updateUserPassword);
+router.delete("/api/users/:userID", verifyToken, deleteUser);
+router.get("/api/users/:userID", verifyToken, getUserById);
+router.get("/api/users/", verifyToken, getAllUsers);
+
 // Job App Routes (Protected)
-
-
-router.get('/api/users/jobApps', getAllJobApps); //Get all Job Apps of user
-router.post('/api/users/jobApps',createJobApp); // Create Job App
-
-
-
-
-
+router.get('/api/users/jobApps', verifyToken, getAllJobApps); // Get all Job Apps of user
+router.post('/api/users/jobApps', verifyToken, createJobApp); // Create Job App
 
 module.exports = router;
