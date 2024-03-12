223c19c62346bbcbfe49548e41d1d166349dcb60
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-11 19:50:18 -0700

    Fixed Auth Bug

--- utils/auth.js
+++ utils/auth.js
@@ -1,28 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    const tokenHeader  = req.headers.authorization;
+    const tokenHeader = req.headers.authorization;
 
     if (!tokenHeader) {
         return res.status(403).json({ message: "No JWT found!" });
     }
+
     const token = tokenHeader.split(' ')[1].trim();
+    
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if (err) {
             console.error('Error verifying token:', err.message);
             return res.status(401).json({ message: 'Unauthorized' });
         }
-        
-        const userId = decoded.userId;
 
-        // Compare userId with expected value (e.g., user ID from database)
-        if (userId !== expectedUserId) {
-            console.error('Invalid userId:', userId);
-            return res.status(401).json({ message: 'Unauthorized' });
-        }
-
-        // If userId matches, proceed to the next middleware or route handler
-        req.userId = userId;
+        // If verification succeeds, attach the decoded user ID to the request object
+        req.userId = decoded.userId;
         next();
     });
 };
