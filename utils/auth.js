f369d2481fef2928ff08ce62f1ce773181beaec8
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-11 19:48:00 -0700

    Added JobApps to DB w routes

--- utils/auth.js
+++ utils/auth.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization;
+    const tokenHeader  = req.headers.authorization;
 
-    if (!token) {
-        return res.status(403).json({ message: 'No JWT found!' });
+    if (!tokenHeader) {
+        return res.status(403).json({ message: "No JWT found!" });
     }
-
+    const token = tokenHeader.split(' ')[1].trim();
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if (err) {
             console.error('Error verifying token:', err.message);
