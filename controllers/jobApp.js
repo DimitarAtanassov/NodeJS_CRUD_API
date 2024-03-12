a795c60a2936367af01895f7282df3c4dd2cef4a
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-12 12:22:27 -0700

    Updated Job Apps get funciton

--- controllers/jobApp.js
+++ controllers/jobApp.js
@@ -2,25 +2,28 @@
 const JobApp = require("../model/jobApp.model");
 
 const createJobApplication = async (req, res) => {
-    try {
-        const { company, title, link } = req.body;
-        const userId = req.userId; // Assuming you're using JWT authentication and the user ID is stored in the request object
+  try {
+      const { company, title, link } = req.body;
+      const userId = req.userId; // Assuming you're using JWT authentication and the user ID is stored in the request object
 
-        // Create a new job application
-        const newJobApp = new JobApp({
-            company,
-            title,
-            link,
-            user: userId // Associate the job application with the user
-        });
+      // Create a new job application
+      const newJobApp = new JobApp({
+          company,
+          title,
+          link,
+          user: userId // Associate the job application with the user
+      });
 
-        // Save the new job application to the database
-        await newJobApp.save();
+      // Save the new job application to the database
+      await newJobApp.save();
 
-        res.status(201).json({ message: "Job application created successfully", jobApp: newJobApp });
-    } catch (error) {
-        res.status(500).json({ message: "Internal server error", error: error.message });
-    }
+      // Update the user's jobApplications array with the ID of the new job application
+      await User.findByIdAndUpdate(userId, { $push: { jobApplications: newJobApp._id } });
+
+      res.status(201).json({ message: "Job application created successfully", jobApp: newJobApp });
+  } catch (error) {
+      res.status(500).json({ message: "Internal server error", error: error.message });
+  }
 };
 
 const getAllJobAppsByUserId = async (req,res) => {
