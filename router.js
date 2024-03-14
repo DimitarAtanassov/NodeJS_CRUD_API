2f12b87f6243d45c90a499c9dad992b325f1ce59
DimitarAtanassov <dimitar789558@gmail.com>
2024-03-13 17:22:54 -0700

    added jwt refresh logic

--- router.js
+++ router.js
@@ -23,6 +23,11 @@ const {verifyToken,refreshToken} = require('./utils/auth');
 
 router.get("/", async (req,res) => {
     res.send("CRUD API FOR MONGODB!");
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
 });
 
 // User routes
